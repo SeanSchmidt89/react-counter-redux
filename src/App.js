da362ca0c73a586e11a510913ba057b82ce03d28
sean <seanschmidt89@yahoo.com>
2022-10-01 05:36:47 -0700

    finished logic on toggle comp

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Header from "./components/header/Header";
 import Display from "./components/display/Display";
 import Input from "./components/input/Input";
 import Toggle from "./components/toggle/Toggle";
+import { useSelector } from "react-redux";
+
 function App() {
+  const toggle = useSelector((state) => state.showCounter);
   return (
     <div className="App">
       <Header />
-      <Display />
-      <Counter />
-      <Input />
+      {toggle && <Display />}
+      {toggle && <Counter />}
+      {toggle && <Input />}
       <Toggle />
     </div>
   );
--- src/components/toggle/Toggle.js
+++ src/components/toggle/Toggle.js
@@ -2,16 +2,26 @@ import React from "react";
 import styles from "./Toggle.module.css";
 import { useDispatch } from "react-redux";
 import { CounterSliceActions } from "../../store/CounterSlice";
+import { useSelector } from "react-redux";
 
 const Toggle = () => {
   const dispatch = useDispatch();
+  const toggle = useSelector((state) => state.showCounter);
   const zeroHandler = () => {
     dispatch(CounterSliceActions.zero());
   };
+
+  const toggleHandler = () => {
+    dispatch(CounterSliceActions.toggle());
+  };
   return (
     <div className={styles.toggle}>
-      <button>Close</button>
-      <button onClick={zeroHandler}>Zero</button>
+      {toggle ? (
+        <button onClick={toggleHandler}>Close</button>
+      ) : (
+        <button onClick={toggleHandler}>Open</button>
+      )}
+      {toggle && <button onClick={zeroHandler}>Zero</button>}
     </div>
   );
 };
